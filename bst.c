698edee0cca452cc84d91f13823de96c385f17ab
Kbcoder54 <114585696+Kbcoder54@users.noreply.github.com>
2022-10-19 13:05:34 +0100

    Add files via upload

--- bst.c
+++ bst.c
@@ -1,14 +1,17 @@
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h> //remove
 #include <assert.h> //remove
 #include "bst.h"
-
+//might need to remove
 struct _Node {
     int value;
     struct _Node *left;
     struct _Node *right;
 
 };
+static Node* realRoot;//might need to remove
 void displaySubtree(Node *N){
     if(N == NULL){
         return;
@@ -19,69 +22,222 @@ void displaySubtree(Node *N){
 
 
 }
-Node* addNode(Node *root, int value){
+Node* newNode(int data){
     Node *temp = (Node *)malloc(sizeof(Node));
-    temp -> value = value;
-    temp -> left = NULL;
-    temp -> right = NULL;
+    if(temp!=NULL){
+       temp -> value = data;
+       temp -> left = NULL;
+       temp -> right = NULL;
+    }
 
-    if (root == NULL){
-        return temp;
+    return temp;
+
+
+}
+
+int Contains(int value){
+    Node* temp = realRoot;
+    while (temp != NULL){
+        // checking condition and passing right subtree & recusing
+        if (value > temp->value)
+            temp = temp->right;
+
+        // checking condition and passing left subtree & recusing
+        else if (value < temp->value)
+            temp = temp->left;
+        else
+            temp = NULL;
+            return 1;
+            // if the value is found return 1
+    }
+    return 0;
+
+}
+Node* addNode(Node *root, int value){
+
+    /*Node* newnode = newNode(value);
+
+
+    Node* x = root;//NULL
+    Node* y = NULL;
+    //Node* temp = newNode(value);
+
+    while (x != NULL) {
+        y = x;
+        if (value < x->value){
+            x = x->right;
+        }
+        else if (value > x->value){
+            x = x->left;
+        }
+        else{
+            x = NULL;
+            break;
+        }
+    }
+
+    if (y == NULL){
+        realRoot = newnode;
+        y = newnode;
+    }
+
+    else if (value > y->value){
+        y->left = newnode;
+    }
+
+    else if (value < y-> value){
+        y->right = newnode;
+    }
+
+    return newnode;*/
+    Node * temp = newNode(value);
+
+
+   Node * i = root;
+   Node * j = NULL;
+   while(i!=NULL){
+        j = i;
+       if(i->value > value){
+          i = i->right;
+       }
+       else if(i->value < value){
+          i = i->left;
+       }
+       else{
+          j = NULL;
+          break;
+       }
+   }
+
+   if(j==NULL){
+      j = temp;
+   }
+   else if(j->value < value){
+      j->left = temp;
+   }
+   else if(j->value > value){
+      j->right = temp;
+   }
+
+   return temp;
+
+
+
+
+
+    /*Node *p;
+    if(root == NULL)
+        p = newNode(value);
+        return newNode(value);
+
+    if(root->value > value)
+        root->right = addNode(root->right,value);
+
+    else if(root->value < value)
+        root->left = addNode(root->left,value);
+
+    return p;*/
+    /*if (root == NULL){
+        return newNode(value);
+    }
+    Node *curr = root;
+    while(true){
+        if(curr->value > value){
+            if(curr -> right != NULL){
+                curr = curr -> right;
+            }
+            else{
+                curr = newNode(value);
+                break;
+            }
+        }
+        else{
+            if(curr -> left != NULL){
+                curr = curr -> left;
+            }
+            else{
+                curr -> left = newNode(value);
+                break;
+            }
+        }
+
+    }
+    return curr;*/
+
+
+
+    /*Node *p = newNode(value);
+    if(root == NULL){
+        realRoot = p;
+        return p;
+    }
+    while(root!=NULL){
+        if(value > root -> value){
+        root -> left = addNode(root->left,value);
+    }
+    else if(value < root -> value){
+        root -> right = addNode(root->right,value);
     }
     else{
-        if(root -> value > value){
-        root -> right = addNode(root->right,value);
+        if(Contains(value)== 1){
+            return NULL;
+            root = NULL;
         }
-        else {
-        root -> left = addNode(root->left,value);
+        else{
+            return p;
         }
     }
-        return root;
-    //return root;
+    }*/
+
+
 
 
 
 }
+
 Node * removeNode(Node * root, int value){
-    if (root == NULL){
+    if(root == NULL){
         return root;
     }
-    if(root->value > value){
-        root -> right = removeNode(root->right,value);
+    if(value<root->value){
+        root = removeNode(root->right,value);
     }
-    else if(root -> value < value){
-        root -> left = removeNode(root->left,value);
+    else if(value > root -> value){
+        root = removeNode(root ->left,value);
     }
     else{
-        //Case 1:if both children are null
-        if(root -> left == NULL && root->right == NULL){
-            free(root);
-            return NULL;
-        }
-        //Case 2:if one child is not null
-        else if (root -> left == NULL || root->right == NULL){
-            Node *temp = root -> left ? root -> left : root -> right; // we want the non null node
+        if(root->left==NULL){
+            Node* temp = root->right;
             free(root);
             return temp;
         }
-        //Case 3:if both children are not null
-        else if(root->left != NULL && root->right != NULL){
-        //find the leftmost element in right sub tree as it contains the smallest value in that subtree
-            Node *temp = root->right;
-            while (temp->left != NULL){
-                temp = temp -> left;
-            }
-            root -> value = temp -> value;
-            root->right = removeNode(root->right, root->value);//repeating case 2
+        else if(root->right==NULL){
+            Node* temp = root->left;
+            free(root);
+            return temp;
         }
+        Node *temp = root -> right;
+        while(temp && temp->left!=NULL){
+            temp = temp->left;
+        }
+        root->value = temp -> value;
+        root -> right = removeNode(root->right,temp->value);
     }
     return root;
+
+
+
+
+
+
+
+
 
 }
 
 int numberLeaves(Node * N){
     if(N == NULL){
-            return 0;
+        return 0;
     }
     if(N ->left == NULL && N -> right == NULL){
         return 1;
@@ -93,53 +249,142 @@ int numberLeaves(Node * N){
 
 
 }
-Node * removeSubtree(Node * root, int value){
-    if(root!=NULL){
+Node * removeSubtree(Node * root, int value){//gives stack error
+    while(root!=NULL){
         removeSubtree(root->left,root->left->value);
         removeSubtree(root->right,root->right->value);
-        free(root);
+        removeNode(root,root->value);
     }
     return root;
 
 
 
 }
-
-
-
-int main(){ //remove
-    /*Node *a, *b,*c;
-
-    a = addNode(NULL, 42);
-    b = addNode(a,3);
-    c = addNode(a,2);
-    displaySubtree(a);
-    c = removeNode(b,3);
-    displaySubtree(c);
-    thus c is equal to a
-    assert(c == a);*/
-
-    /*int num = numberLeaves(b);*/
-    Node *a, *b;
-  a = addNode(NULL, 42);
-  b = removeNode(a, 42);
-  assert(b == NULL);
-
-  a = addNode(NULL, 1);
-  b = addNode(a, 2);
-
-  displaySubtree(a);
-  int c = numberLeaves(a);
-  assert(c == 1);
-
-  //c = nodeDepth(a,b);
-  //assert(c == 1);
-
-  a = removeSubtree(b, 2);
-  displaySubtree(a);
+bool partofsubtree(Node* N,int value){ //remove
+    if(N==NULL){
+        return false;
+    }
+    if(N->value == value){
+        return true;
+    }
+    bool node1 = partofsubtree(N->left,value);
+    if(node1){
+        return true;
+    }
+    bool node2 = partofsubtree(N->right,value);
+    if(node2){
+        return true;
+    }
+    return false;
 
 
 }
+int sub(Node *R,int value){
+    if(R == NULL){
+        return 0;
+    }
+    else if(R->value == value){
+        return 1;
+    }
+    else if(R->value > value){
+        return sub(R->right,value);
+    }
+    else{
+        return sub(R->left,value);
+    }
+
+
+}
+int min(int a, int b){
+    if(a > b) return b;
+    return a;
+}
+int nodeDepth (Node * R, Node * N){
+    /*if(R == NULL){
+        return 0;
+    }
+    int left_depth= nodeDepth(R->left,N->left);
+    int right_depth= nodeDepth(R->right,N->right);
+    if(left_depth > right_depth)
+        return left_depth +1;
+    else
+        return right_depth +1;*/
+    /*if(R == NULL){
+        return 0;
+    }
+    if(R->left == NULL && R->right == NULL){
+        return 1;
+    }*/
+    /*if(R->left == 0)
+        return nodeDepth(R->right,N -> right)+1;
+    if(R -> right ==0)
+        return nodeDepth(R->right,N -> right) + 1;*/
+    /*int l = nodeDepth(R->right,N -> right),r = nodeDepth(R->right,N -> right);
+    return l > r ? r+1 : l+1;*///find the min
+
+
+    /*int depth = 0;
+    if(R==NULL){
+        return depth;
+    }
+    if(R -> value == N -> value){
+        depth = -1;
+        return depth;
+    }
+    if(R -> value == N -> value){
+        return depth;
+    }
+    else{
+        while (R != NULL){
+
+        if (N -> value > R->value){
+            R = R->right;
+            depth++;
+        }
+        else if (N -> value < R->value){
+            R = R->left;
+            depth++;
+        }
+        else{
+            return depth;
+        }
+
+     }
+    }*/
+    int depth = 0;
+    int found = 0;
+    int nVal = N->value;
+    Node* curr = R;
+    while(R!=NULL){
+        if(nVal > R -> value){
+            R = R -> left;
+            depth++;
+        }
+        else if(nVal < R -> value){
+            R = R -> right;
+            depth++;
+        }
+        else{
+            found = 1;
+            R = NULL;
+        }
+    }
+    if(found == 0){
+        return -1;
+    }
+    else{
+        return depth;
+    }
+
+
+    }
+
+
+
+
+
+
+
 
 
 
--- test_bst.c
+++ test_bst.c
@@ -1,25 +1,48 @@
 #include <stdio.h>
 #include <assert.h>
 
+
 #include "bst.h"
+#include "bst.c" //remove
+
 
 int main() {
-  Node *a, *b;
-  a = addNode(NULL, 42);
-  b = removeNode(a, 42);
-  assert(b == NULL);
+    /*Node *n1, *n2, *n3,*n4;
+    n1 = addNode(NULL, 5);
+    n2 = newNode(8);
+    n3 = newNode(4);
+    n4 = newNode(10);
+    n1->left = n2;
+    n1->right = n3;
+    n2->left = n4;
+    int c = nodeDepth(n1,n4);
+    assert(c == 2);*/
+   Node *rootNode, *b, *c, *d, *e, *f, *g, *h, *i, *j, *k, *z;
+  rootNode = addNode(NULL, 34);
+  b = addNode(rootNode, 53);
+  c = addNode(rootNode, 31);
+  d = addNode(rootNode, 5);
+  e = addNode(rootNode, 2);
+  f = addNode(rootNode, 3);
+  g = addNode(rootNode, 23);
+  h = addNode(rootNode, 1);
+  i = addNode(rootNode, 56);
+  j = addNode(rootNode, 12);
+  k = addNode(rootNode, 91);
+  z = addNode(rootNode, 35);
 
-  a = addNode(NULL, 1);
-  b = addNode(a, 2);
+  assert(numberLeaves(rootNode) == 5);
+  removeSubtree(rootNode, 5);
+  assert(nodeDepth(rootNode, k) == 3);
+  assert(c->left == NULL);
+  assert(c->right == NULL);
+  assert(nodeDepth(b, k) == 2);
+  assert(nodeDepth(i, k) == 1);
+  assert(removeSubtree(rootNode, 34) == NULL);
 
-  displaySubtree(a);
-  int c = numberLeaves(a);
-  assert(c == 1);
 
-  c = nodeDepth(a,b);
-  assert(c == 1);
 
-  a = removeSubtree(a, 2);
-  
-  return 0;
+
+    return 0;
+
 }
